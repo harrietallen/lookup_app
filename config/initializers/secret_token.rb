11cad83e65e718f7f4154ae5eee554baeb7369b6
Harriet Allen <hallen@madisoncollege.edu>
2013-10-13 11:43:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LookupApp::Application.config.secret_key_base = 'b722b3831a13a11c441af3c2683bb67a8f4bd474877d90f62785599a982f84ce9d7b96e1eb62a7333e8677ee8df759695b3c491427c37686cd113ae5a30d94fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LookupApp::Application.config.session_store :cookie_store, key: '_lookup_app_session'
